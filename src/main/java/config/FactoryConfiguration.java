fdf12768e4b242d329fbb1abc0f65117d7f8e287
L.K.H.M Lakdiw <manuthlakdiv2006@gmail.com>
2025-02-07 10:32:50 +0530

    boostraped

--- src/main/java/config/FactoryConfiguration.java
+++ src/main/java/config/FactoryConfiguration.java
@@ -1,4 +1,14 @@
 package config;
 
+import entity.Customer;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfiguration {
+    // hibernate boostrap karanna thamai meken karanne (start karanna)
+
+    public FactoryConfiguration(){
+       Configuration configuration =  new Configuration().configure(); // meka dunna gaman xml file eka process wenawa (load wenawa)
+        configuration.addAnnotatedClass(Customer.class); // class eka set karanwa (hadana hadana entities methenta add karannona)
+        configuration.buildSessionFactory();
+    }
 }
