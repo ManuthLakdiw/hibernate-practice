09f0c8c0bf47ea3b1c0ec265d41d737fbe852266
L.K.H.M Lakdiw <manuthlakdiv2006@gmail.com>
2025-02-07 11:38:17 +0530

    created delete customer

--- src/main/java/lk/ijse/hibernate/Main.java
+++ src/main/java/lk/ijse/hibernate/Main.java
@@ -18,6 +18,12 @@ public class Main {
         );
 
 
+        Customer customer1 = getCustomer(1);
+        System.out.println(customer1);
+
+        Customer customer2 = getCustomer(5);
+        System.out.println(customer2);
+
 //        boolean check = saveCustomer(customer);
 //        if (check) {
 //            System.out.println("Customer has been saved");
@@ -26,14 +32,23 @@ public class Main {
 //            System.out.println("Customer has not been saved");
 //        }
 
-        Customer customer1 = getCustomer(1);
-        System.out.println(customer1);
-
-        Customer customer2 = getCustomer(5);
-        System.out.println(customer2);
+        boolean check = deleteCustomer(1);
+        if (check) {
+            System.out.println("customer deleted successfully");
+        }else {
+            System.out.println("customer not deleted successfully");
+        }
 
 
     }
+
+    public static Customer getCustomer(int id){
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Customer customer = session.get(Customer.class, id);
+        session.close();
+        return  customer;
+    }
+
 
     public static boolean saveCustomer(Customer customer){
         Session session = FactoryConfiguration.getInstance().getSession();
@@ -53,12 +68,22 @@ public class Main {
         }
     }
 
-    public static Customer getCustomer(int id){
+    public static boolean deleteCustomer(int id){
         Session session = FactoryConfiguration.getInstance().getSession();
-        Customer customer = session.get(Customer.class, id);
-        session.close();
-        return  customer;
+
+        try {
+            Transaction transaction = session.beginTransaction();
+            Customer customer = session.get(Customer.class, id);
+            session.delete(customer);
+            transaction.commit();
+            return true;
+        }catch (Exception e){
+            return false;
+        }finally {
+            session.close();
+        }
     }
+
 
 
 }
