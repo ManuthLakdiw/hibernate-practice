a8af785fa3d73f7d9308d8a528e2c0ccde13ca6b
L.K.H.M Lakdiw <manuthlakdiv2006@gmail.com>
2025-02-07 10:56:27 +0530

    updated

--- src/main/java/lk/ijse/hibernate/Main.java
+++ src/main/java/lk/ijse/hibernate/Main.java
@@ -1,17 +1,30 @@
 package lk.ijse.hibernate;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import config.FactoryConfiguration;
+import entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Customer customer = new Customer(
+                2,
+                "lakdiv",
+                "lakdiw@gmail.com"
+        );
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(customer);
+
+        transaction.commit();
+
+        session.close();
+
+
     }
 }
