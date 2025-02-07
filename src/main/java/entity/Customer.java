d67d113d76b2172ced7a40ba8ad3471c78bda627
L.K.H.M Lakdiw <manuthlakdiv2006@gmail.com>
2025-02-07 09:39:52 +0530

    mapped new table name and pk

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -1,13 +1,22 @@
 package entity;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Data // meka athule getter , setter , toString thiyenawa
 @AllArgsConstructor
 @NoArgsConstructor
-public class Customer {
-    private int id;
+
+@Entity // map to table
+
+@Table(name = "customer_table") // default table name eka wenuwat name eka wenas kara customer_table kiyala
+public class Customer {  // default table name eka customer meka wenas karanna puluwan
+
+    @Id
+    private int id; // set this variable for table primary key
     private String name;
     private String email;
 }
