6cdba77611fb6d9904f3f06db714fbc4fcab97f3
L.K.H.M Lakdiw <manuthlakdiv2006@gmail.com>
2025-02-07 10:45:38 +0530

    updated

--- src/main/java/config/FactoryConfiguration.java
+++ src/main/java/config/FactoryConfiguration.java
@@ -1,14 +1,36 @@
 package config;
 
 import entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class FactoryConfiguration {
     // hibernate boostrap karanna thamai meken karanne (start karanna)
 
-    public FactoryConfiguration(){
+
+    //mulu project ekakata session factory eka parai hadenne eka nisa singleton desgin pattern eka danwa
+
+    private static FactoryConfiguration factoryConfiguration;
+
+    private static SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
        Configuration configuration =  new Configuration().configure(); // meka dunna gaman xml file eka process wenawa (load wenawa)
         configuration.addAnnotatedClass(Customer.class); // class eka set karanwa (hadana hadana entities methenta add karannona)
         configuration.buildSessionFactory();
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance(){
+        if(factoryConfiguration == null){
+            factoryConfiguration = new FactoryConfiguration();
+        }
+        return factoryConfiguration;
+    }
+
+//    hema crud ekakatama session open karanna ona nisa meken session return karagannawa
+    public Session getSession() {
+        return sessionFactory.openSession();
     }
 }
